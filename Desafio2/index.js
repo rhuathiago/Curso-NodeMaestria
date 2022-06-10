88facbe8930717524e1ddd0b54bd8db306677353
rhuathiago <rhuaalcantara@gmail.com>
2022-06-10 12:43:46 -0300

    up

---
+++ Desafio2/index.js
@@ -0,0 +1,19 @@
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+
+inquirer
+    .prompt([
+        { name: 'nome', message: 'Qual o seu nome?' },
+        { name: 'idade', message: 'Qual a sua idade?' }
+    ])
+    .then((answers) => {
+
+        if (!answers.nome || !answers.idade) {
+            throw new Error('O nome e a idade são obrigatórios!')
+        }
+
+        const response = `O nome do usuário é ${answers.nome} e ele tem ${answers.idade} anos de idade`
+
+        console.log(chalk.bgYellow.black(response))
+    })
+    .catch((err) => console.log(err))
