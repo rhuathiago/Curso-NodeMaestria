af26914868a5325b148bfa379db52883f6a70289
rhuathiago <rhuaalcantara@gmail.com>
2022-06-18 21:28:20 -0300

    up

--- 3_EXPRESS/Tarefa03/index.js
+++ 3_EXPRESS/Tarefa03/index.js
@@ -1,41 +1,25 @@
 const express = require('express')
-const server = express()
+const app = express()
 const port = 5000
 
 const path = require('path')
 
-server.use(
+const users = require('./users')
+
+app.use(
     express.urlencoded({
         extended: true,
     }),
 )
 
-server.use(express.json())
+app.use(express.json())
+
+app.use(express.static('public'))
 
 const basePath = path.join(__dirname, 'templates')
 
-server.get('/users/add', (req, res) => {
-    res.sendFile(`${basePath}/userform.html`)
-})
+app.use('/users', users)
 
-server.post('/users/save', (req, res) => {
-
-    console.log(req.body)
-
-})
-
-server.get('/users/:id', (req, res) => {
-    const id = req.params.id
-    
-    console.log(`Procurando o usuário: ${id}`)
-    
-    res.sendFile(`${basePath}/users.html`)
-})
-
-server.get('/', (req, res) => {
-    res.sendFile(`${basePath}/index.html`)
-})
-
-server.listen(port, () => {
-    console.log(`Server está rodando na porta ${port}`)
+app.listen(port, () => {
+    console.log(`O server está rodando na porta ${port}`)
 })
--- 3_EXPRESS/Tarefa03/users/index.js
+++ 3_EXPRESS/Tarefa03/users/index.js
@@ -6,11 +6,38 @@ router.use(express.json())
 
 const basePath = path.join(__dirname, '../templates')
 
-// router.get('/id', (req, res) => {
-//     const id = req.params.id
-//     res.sendFile(`${basePath}/users.html`)
 
-//     console.log(`O id do usuário é ${id}`)
-// })
+
+router.get('/add', (req, res) => {
+    res.sendFile(`${basePath}/userform.html`)
+})
+
+router.post('/save', (req, res) => {
+
+    console.log(req.body)
+
+    const name = req.body.name
+    const age = req.body.age
+
+    console.log(`O nome do usuário é ${name} e a sua idade é ${age} anos`)
+
+    res.sendFile(`${basePath}/userform.html`)
+})
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id
+    
+    console.log(`Procurando o usuário: ${id}`)
+    
+    res.sendFile(`${basePath}/users.html`)
+})
+
+router.get('/', (req, res) => {
+    res.sendFile(`${basePath}/index.html`)
+})
+
+router.use(function (req, res, next){
+    res.status(404). sendFile(`${basePath}/404.html`)
+})
 
 module.exports = router
